8af1645f689652792817761e195d165663a01e6a
Casey Richardson <caseyrichardson01@gmail.com>
2012-10-03 12:13:45 -0500

    Initial Commit

---
+++ CSLAB/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  CSLAB
+//
+//  Created by Casey Richardson on 10/3/12.
+//  Copyright (c) 2012 Casey Richardson. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ CSLAB/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CSLAB
+//
+//  Created by Casey Richardson on 10/3/12.
+//  Copyright (c) 2012 Casey Richardson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
